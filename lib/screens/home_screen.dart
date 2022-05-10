4f9ae4e503db8bdd5dacc2428cde6eac196fc178
leodrumers <leodrumers@gmail.com>
2022-05-10 09:46:38 -0500

    Using Swiper in homeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:movies/widgets/index.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movies'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
       ),
+      body: Column(children: const [CardSwiper()]),
     );
   }
 }
