332695f889282b0ff9dd7b430f80e02f39a091a5
leodrumers <leodrumers@gmail.com>
2022-05-12 08:57:55 -0500

    MovieSlider created and indexed

---
+++ lib/widgets/movie_slider.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class MovieSlider extends StatelessWidget {
+  const MovieSlider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 290,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'Popurlar movies',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 20,
+              itemBuilder: (_, index) => const _MoviePoster(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MoviePoster extends StatelessWidget {
+  const _MoviePoster({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 190,
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details'),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/300x400'),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Un titulo super hiper mega largo largo',
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
