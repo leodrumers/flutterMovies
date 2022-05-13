f8047542dbc0b4b48b86b7566e795e41d4da7e0f
leodrumers <leodrumers@gmail.com>
2022-05-13 07:50:27 -0500

    Using movie widgets in details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:movies/widgets/index.dart';
 
 class DeatailsScreen extends StatelessWidget {
   const DeatailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          MoviesAppBar(),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            MoviePosterAndTitle(),
+            MovieOverView(),
+            MovieCastingCard(),
+          ])),
+        ],
       ),
     );
   }
