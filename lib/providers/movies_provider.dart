699d359ab4ebedcc4bf59dc0f7865ac7f707f56e
leodrumers <leodrumers@gmail.com>
2022-05-13 10:03:37 -0500

    PopularMovies method created

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -4,14 +4,16 @@ import 'package:http/http.dart' as http;
 import 'package:movies/models/index.dart';
 
 class MoviesProvider extends ChangeNotifier {
-  String _baseUrl = 'api.themoviedb.org';
-  String _apiKey = 'ab0f52993856c0065d1a7b7d831007b1';
-  String _language = 'en-US';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _apiKey = 'ab0f52993856c0065d1a7b7d831007b1';
+  final String _language = 'en-US';
 
   List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
 
   MoviesProvider() {
     getOnDisplayMovies();
+    getPopularMovies();
   }
 
   getOnDisplayMovies() async {
@@ -19,8 +21,17 @@ class MoviesProvider extends ChangeNotifier {
         {'api_key': _apiKey, 'language': _language});
 
     final response = await http.get(url);
-    final playingResponse = PlayingResponse.fromJson(response.body);
-    onDisplayMovies = playingResponse.movies;
+    onDisplayMovies = PlayingResponse.fromJson(response.body).movies;
+
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    var ulr = Uri.https(_baseUrl, '3/movie/popular',
+        {'api_key': _apiKey, 'language': _language});
+
+    final response = await http.get(ulr);
+    popularMovies = PlayingResponse.fromJson(response.body).movies;
 
     notifyListeners();
   }
