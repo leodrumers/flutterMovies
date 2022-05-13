cc2cd31d8e2427352bb248963dc09aee317fd4fb
leodrumers <leodrumers@gmail.com>
2022-05-13 10:04:02 -0500

    ProgressIndicator when movies list is empty

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -10,6 +10,15 @@ class CardSwiper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
+    if (movies.isEmpty) {
+      return Container(
+        width: double.infinity,
+        height: size.height * .5,
+        child: const Center(child: CircularProgressIndicator()),
+      );
+    }
+
     return Container(
       width: double.infinity,
       height: size.height * .5,
