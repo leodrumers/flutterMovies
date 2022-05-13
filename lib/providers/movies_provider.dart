197947848e9e3f20dc752027b44ea3eb697ab6cb
leodrumers <leodrumers@gmail.com>
2022-05-13 08:53:46 -0500

    MoviesProvider created and exported

---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:movies/models/index.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  String _baseUrl = 'api.themoviedb.org';
+  String _apiKey = 'ab0f52993856c0065d1a7b7d831007b1';
+  String _language = 'en-US';
+
+  MoviesProvider() {
+    print('MoviesProvider intanciated');
+    getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language});
+
+    final response = await http.get(url);
+    final playingResponse = PlayingResponse.fromJson(response.body);
+    print(playingResponse.movies[0].originalTitle);
+  }
+}
