6c1d8b582ccb66b2164bab2e2271bba928890aa6
leodrumers <leodrumers@gmail.com>
2022-05-13 07:49:33 -0500

    MovieCastingCard created

---
+++ lib/widgets/movie_casting_card.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MovieCastingCard extends StatelessWidget {
+  const MovieCastingCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      width: double.infinity,
+      height: 200,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, index) => CastingCard(),
+        itemCount: 10,
+      ),
+    );
+  }
+}
+
+class CastingCard extends StatelessWidget {
+  const CastingCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      width: 100,
+      height: 150,
+      child: Column(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/no-image.jpg'),
+            image: NetworkImage('https://via.placeholder.com/150x300'),
+            fit: BoxFit.cover,
+            width: 100,
+            height: 140,
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(
+          'movie.actoname loereifenf',
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+          textAlign: TextAlign.center,
+        ),
+      ]),
+    );
+  }
+}
