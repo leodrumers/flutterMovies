c2e60562ff1cbb9b993cfe12d3866e0ca8bdeb51
leodrumers <leodrumers@gmail.com>
2022-05-13 07:50:06 -0500

    Exporting movies widgets

--- lib/widgets/index.dart
+++ lib/widgets/index.dart
@@ -1,2 +1,6 @@
-export 'package:movies/widgets/card_swiper.dart';
-export 'package:movies/widgets/movie_slider.dart';
+export './card_swiper.dart';
+export './details_custom_app_bar.dart';
+export './movie_casting_card.dart';
+export './movie_overview.dart';
+export './movie_poster_and_title.dart';
+export './movie_slider.dart';
