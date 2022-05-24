88f770f5f6e82a0d317c3e2863dc45566b1fd1ad
leodrumers <leodrumers@gmail.com>
2022-05-24 08:17:58 -0500

    Models for credits created

--- lib/models/index.dart
+++ lib/models/index.dart
@@ -1,2 +1,4 @@
+export 'package:movies/models/cast.dart';
+export 'package:movies/models/credit.dart';
 export 'package:movies/models/movie.dart';
 export 'package:movies/models/playing_resopnse.dart';
